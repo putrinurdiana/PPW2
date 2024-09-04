cdd52e3a58c83434cd6ac464c5a88040935d4dc9
putrinudrdiana <putrinurdiana@mail.ugm.ac.id>
2024-09-04 09:32:53 +0700

    initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('title', 'About Us')
+
+@section('content')
+    <h1>About</h1>
+    <p>Nama: Putri Nurdiana</p>
+    <p>NIM: 23/514785/SV/22424</p>
+    <p>Kelas: PL3B2</p>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/about', function () {
+//     return view('about', [
+//         'name' => 'Putri Nurdiana',
+//         'email' => 'putinurdiana@mail.com']);
+// });
+
+
+// Route::get('/tambah', function () {
+//     return view('tambah', [
+//         'name' => 'Putri Nurdiana',
+//         'email' => 'putinurdiana@mail.com']);
+// });
+
+// Route::get('/contoh', function () {
+//     return view('contoh'
+//     );
+// });
+
+// Route::get('/app', function () {
+//     return view('app'
+//     );
+// });
+
+// Route::get('/navbar', function () {
+//     return view('navbar');
+// });
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
+
+// Route::get('/services', function () {
+//     return view('services');
+// });
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+// Route::get('/layout', function () {
+//     return view('layout');
+// });
+
+Route::get('/posts', [PostController::class, 'index']);
+
